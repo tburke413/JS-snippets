d9a80912f82334c2f67274c0a01f3db1c17f283b
Tom's Personal <thomasbwsu@gmail.com>
2020-08-29 22:11:29 -0400

    Nodeschool - Javascripting

---
+++ javascripting_nodeschool/18-functions.js
@@ -0,0 +1,5 @@
+function eat (str1){
+  return str1 + " tasted really good."
+};
+
+console.log(eat('bananas'))
---
+++ javascripting_nodeschool/9-if.js
@@ -0,0 +1,5 @@
+let fruit = 'orange'
+
+fruit.length > 5 ? 
+  console.log('The fruit name has more than five characters.') 
+  : console.log('The fruit name has five characters or less.')
